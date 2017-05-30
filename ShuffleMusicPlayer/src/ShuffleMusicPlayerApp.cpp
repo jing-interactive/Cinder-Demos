7bffbebf552670b0f02455354d19a043f65d1ce9
vinjn <vinjn.z@gmail.com>
2017-05-30 21:48:25 +0800

    New project - ShuffleMusicPlayer

---
+++ ShuffleMusicPlayer/src/ShuffleMusicPlayerApp.cpp
@@ -0,0 +1,41 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Log.h"
+
+#include "AssetManager.h"
+#include "MiniConfig.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class ShuffleMusicPlayerApp : public App
+{
+  public:
+    void setup() override
+    {
+        log::makeLogger<log::LoggerFile>();
+        createConfigUI({200, 200});
+
+        mCurrentVoice = am::voice("dioramaloop.mp3");
+        mCurrentVoice->start();
+    
+        getWindow()->getSignalKeyUp().connect([&](KeyEvent& event) {
+            if (event.getCode() == KeyEvent::KEY_ESCAPE) quit();
+        });
+        
+        getWindow()->getSignalDraw().connect([&] {
+            gl::clear();
+        });
+    }
+    
+private:
+    audio::VoiceRef mCurrentVoice;
+};
+
+CINDER_APP( ShuffleMusicPlayerApp, RendererGl, [](App::Settings* settings) {
+    readConfig();
+    settings->setWindowSize(APP_WIDTH, APP_HEIGHT);
+    settings->setMultiTouchEnabled(false);
+} )
